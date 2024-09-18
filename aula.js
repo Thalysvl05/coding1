925c47dbe8f85d6c9a02432ee0f4ecddad09cb3b
Thalysvl05 <Thalysvl05@gmail.com>
2024-09-18 18:00:25 +0000

    criamos algaritmo boletim

---
+++ aula.js
@@ -0,0 +1,82 @@
+
+ //CALCULOS BASICOS
+
+/*const { Console } = require("console")
+
+const numero1 = 10;
+const numero2 = 70;
+
+const soma = numero1 + numero2;
+const multiplicacao = numero1 * numero2;
+const divisao = numero1 / numero2;
+const subtracao = numero1 - numero2;
+
+console.log ('o resultado da soma é' + ' ' + soma);
+console.log ('o resultado da multiplicação é' + ' ' + multiplicacao);
+console.log ('o resultado da divisão é' + ' ' + divisao);
+console.log ('o resultado da subtração é' + ' ' + subtracao);*/
+
+ //FRASE BASICA
+
+/*const idade = 19;
+const nome = "Thalys";
+const cidade = "paulista"
+
+console.log ("Olá, meu nome é " + [nome] + " e tenho " + [idade] + " anos de idade e moro em " + [cidade]);*/
+
+ //AREA DE UM TRIANGULO
+
+/*const base = 30
+const altura = 20
+const area = base * altura / 2
+
+console.log ('A área do triângulo é ' + area)*/
+
+ //MEDIA ARITMETICA
+
+/*const nota1 = 8;
+const nota2 = 10;
+const nota3 = 5;
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log ('A média de Thalys é ' + media);*/
+
+ //DESCONTO SOBRE PRODUTO
+
+/*const preco_original = 100
+const desconto = 5
+const preco_final = preco_original * (1 - desconto / 100)
+
+console.log ('preço final = ' + preco_final)*/
+
+ //CONVERSOR DE TEMPERATURA
+
+/*const celsius = 28
+const fahrenheit = (celsius * 9/5) + 32
+
+console.log ('O valor da converção é ' + fahrenheit)*/
+
+ //TABELA IMC
+
+/*const peso = 80
+const altura = 180
+const imc = peso / (altura * altura)
+
+console.log ('o IMC é: ' + imc.toFixed(4))*/
+
+ //IMPOSTO DE RENDA
+
+/*const salario_base = 1.412
+const aliquota = 0.075
+const valor_final = salario_base - (salario_base * aliquota)
+
+console.log ('O valor final do imposto de renda é ' + valor_final.toFixed(3))*/
+
+ //CONVERSOR DE MOEDA
+
+/*const real = 500
+const dolar = 5.45
+const conversao = real / dolar
+
+
+console.log ('o valor da conversão é ' + real + ' ' + dolar.toFixed(2))/*
